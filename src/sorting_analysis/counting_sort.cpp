6e609ca894c75d8abde823ff99380e27ef57d9b3
FaaizHaikal <faaizhaikal@gmail.com>
2024-05-16 22:47:11 +0700

    feat: finish counting sort

--- src/sorting_analysis/counting_sort.cpp
+++ src/sorting_analysis/counting_sort.cpp
@@ -1,5 +1,30 @@
 #include "sorting_analysis/counting_sort.hpp"
 
 void CountingSort::sort() {
-  // Write your code here
+  int n = data.size();
+
+  int max = INTMAX_MIN;
+
+  for (int i = 0; i < n; i++) {
+    max = std::max(data[i], max);
+  }
+
+  std::vector<int> count(max + 1, 0);
+
+  for (int i = 0; i < n; i++) {
+    count[data[i]]++;
+  }
+
+  for (int i = 0; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
+
+  std::vector<int> output(n);
+
+  for (int i = n - 1; i >= 0; i--) {
+    output[count[data[i]] - 1] = data[i];
+    count[data[i]]--;
+  }
+
+  data = output;
 }
