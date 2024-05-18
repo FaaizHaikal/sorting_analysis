c45c85de10b282519705bdf76027c991d6f3e3ff
amadheatrizza <amadheatrizza@gmail.com>
2024-05-19 00:37:29 +0700

    finish insertion sort

--- src/sorting_analysis/insertion_sort.cpp
+++ src/sorting_analysis/insertion_sort.cpp
@@ -1,5 +1,16 @@
 #include "sorting_analysis/insertion_sort.hpp"
 
 void InsertionSort::sort() {
-  // Write your code here
+    int n = data.size();
+
+    for (int i = 1; i < n; i++) {
+        int key = data[i];
+        int j = i - 1;
+        
+        while (j >= 0 && data[j] > key) {
+            data[j + 1] = data[j];
+            j = j - 1;
+        }
+        data[j + 1] = key;
+    }
 }
