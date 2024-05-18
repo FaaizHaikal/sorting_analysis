0e6453063306dc2daea725f998ca27d82243faaf
amadheatrizza <amadheatrizza@gmail.com>
2024-05-19 00:37:03 +0700

    finish merge sort

--- include/sorting_analysis/merge_sort.hpp
+++ include/sorting_analysis/merge_sort.hpp
@@ -2,11 +2,16 @@
 #define SORTING_ANALYSIS__MERGE_SORT_HPP_
 
 #include "sorting_analysis/sorting.hpp"
+#include <vector>
 
 class MergeSort : public Sorting {
   public:
       MergeSort() : Sorting("Merge Sort") {};
       void sort() override;
+
+  private:
+      void merge_sort(std::vector<int>& arr, int left, int right);
+      void merge(std::vector<int>& arr, int left, int mid, int right);
 };
 
 #endif
